6a016f171e44a17ae379aa2805b81ec5cc19eb74
Lisa Lund <lisa.lund93@gmail.com>
2014-05-08 15:21:31 +0200

    småändringar.

--- src/PongPanel.java
+++ src/PongPanel.java
@@ -1,7 +1,8 @@
 
 import java.awt.Color;
 import java.awt.Graphics;
-
+import java.awt.Graphics2D;
+import java.awt.BasicStroke;
 import javax.swing.JPanel;
 
 public class PongPanel extends JPanel{
@@ -15,14 +16,25 @@ public class PongPanel extends JPanel{
 	}
 	
 	/**
-	 * Paints the rectangle that limits the playing area
-	 * this rectangle is 100 px shorter and narrower than the main window.
+	 * Paints the rectangle that limits the playing area and the dotted line
+	 * that separates the two halves.
+	 * the rectangle is 100 px shorter and narrower than the main window.
 	 */
 	public void paintComponent(Graphics g){
 		
 		super.paintComponent(g);
 		g.setColor(Color.WHITE);
 		g.drawRect(70, 75, 650, 450);
+		
+		paintLine(g, ((70+650)/2), );
+		
+	}
+	
+	/**
+	 * Creates a dotted line from point x to point y.
+	 */
+	public void paintLine(Graphics g, int x, int y){
+		
 	}
 	
 }
